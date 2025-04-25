655c99e54772888993dc08199060fa3436b1ddfc
Tevin <38287265+saphby@users.noreply.github.com>
2025-04-25 12:48:08 +0300

    Add Round Logic

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 function getComputerChoice() {
-    let ranNum = Math.random()
-    console.log(ranNum)
+    let ranNum = Math.random()    
     if (ranNum < 1/3) {
-        console.log(`Rock`)
+        return `Rock`
     } else if ((ranNum > 1/3) && (ranNum < 2/3)) {
-        console.log(`Paper`)
+        return `Paper`
     } else if (ranNum > 2/3) {
-        console.log(`Scissors`)
+        return `Scissors`
     }
 }
 
 function getHumanChoice() {
-    let question = prompt(`Rock, Paper or Scissors`)
-    console.log(question) 
+    let question = prompt(`Rock, Paper or Scissors`)     
     return question    
 }
-getComputerChoice()
-getHumanChoice()
+
+ 
+function playRound(humanChoice, computerChoice) {    
+    humanChoice = getHumanChoice()
+    computerChoice = getComputerChoice()
+    let humanLower = humanChoice.toLowerCase()
+    if (humanLower == `rock` && computerChoice == `Scissors`) {
+        humanScore++
+        console.log(`You Win! Rock DESTROYS Scissors`) 
+    } else if (humanLower == `scissors` && computerChoice == `Rock`) {
+        computerScore++
+        console.log(`You Lose! Rock DESTROYS Scissors`)
+    } else if (humanLower == `paper` && computerChoice == `Rock` ) {
+        humanScore++
+        console.log(`You Win! Paper SUFFOCATES Rock`)
+    } else if (humanLower == `rock` && computerChoice == `Paper`) {
+        computerScore++
+        console.log(`You Lose! Paper SUFFOCATES Rock`)
+    } else if (humanLower == `scissors` && computerChoice == `Paper`) {
+        humanScore++
+        console.log(`You Win! Scissors SLICE & DICE Paper`)  
+    } else if (humanLower == `paper` && computerChoice == `Scissors`) {
+        computerScore++
+        console.log(`You Lose! Scissors SLICE & DICE Paper`)
+    } else if (humanLower == computerChoice.toLowerCase()) {        
+        console.log(`Absolute DooDoo! How u draw with a comp`)
+    }
+       
+} 
+let humanScore = 0
+let computerScore = 0
+
+playRound()
+
